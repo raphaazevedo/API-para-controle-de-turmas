f7c09d58610ba6556979eaf16b340627763bb717
raphaazevedo <azevedo.almeida.ph@gmail.com>
2024-02-11 14:36:36 -0300

    first commit - já com lombok e postgre sql instalado no pom.xml

---
+++ src/main/java/br/com/rapha/ApiControleTurmasApplication.java
@@ -0,0 +1,13 @@
+package br.com.rapha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiControleTurmasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiControleTurmasApplication.class, args);
+	}
+
+}
