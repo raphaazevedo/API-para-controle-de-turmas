51fe57dc1e9c2be2fee31892e87fa8c3d117ad79
raphaazevedo <azevedo.almeida.ph@gmail.com>
2024-02-13 14:54:29 -0300

    feat: criação dos repositories V.1

--- src/main/java/br/com/rapha/repositories/MatriculaRepository.java
+++ src/main/java/br/com/rapha/repositories/MatriculaRepository.java
@@ -1,20 +1,43 @@
 package br.com.rapha.repositories;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.util.UUID;
 
 import br.com.rapha.entities.Matricula;
 import br.com.rapha.factories.ConnectionFactory;
 
 public class MatriculaRepository {
 
-	public void insertMaticula(Matricula matricula)throws Exception{
+	public void insertMaticula(Matricula matricula, UUID turma_id, UUID aluno_id)throws Exception{
 
 		Connection connection = ConnectionFactory.getConnection();
-		// TODO
+
+		PreparedStatement statement = connection.prepareStatement
+				("INSERT INTO matricula (id_matricula, turma_id, aluno_id, data_matricula) VALUES (?, ?, ?, ?)");
+		
+		statement.setObject(1, matricula.getId_matricula());
+		statement.setObject(2, turma_id);
+		statement.setObject(3, aluno_id);
+		statement.setObject(4, matricula.getData_matricula());
+		
+		statement.execute();
+		
 		
 		connection.close();
 	}
 	public void deleteMatricula(Matricula matricula)throws Exception{
-		// TODO
+		
+		Connection connection = ConnectionFactory.getConnection();
+		
+		PreparedStatement statement = connection.prepareStatement
+				("DELETE FROM matricula WHERE id_matricula = ?");
+		
+		statement.setObject(1, matricula.getId_matricula());
+		
+		statement.execute();
+		
+		connection.close();
+		
 	}
 }
--- src/main/java/br/com/rapha/repositories/TurmaRepository.java
+++ src/main/java/br/com/rapha/repositories/TurmaRepository.java
@@ -1,21 +1,123 @@
 package br.com.rapha.repositories;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+import br.com.rapha.entities.Professor;
 import br.com.rapha.entities.Turma;
+import br.com.rapha.factories.ConnectionFactory;
 
 public class TurmaRepository {
-	public void insertTurma(Turma turma)throws Exception{
-		// TODO
+	public void insertTurma(Turma turma, Professor professor)throws Exception{
+
+
+		Connection connection = ConnectionFactory.getConnection();
+		
+		PreparedStatement statement = connection.prepareStatement
+				("INSET INTO turma (id_turma, nome_turma, data_inicio, data_fim, id_professor) VALUES (?,?,?,?,?)");
+		statement.setObject(1, turma.getId_turma());
+		statement.setObject(2, turma.getNome_turma());
+		statement.setObject(3, turma.getData_inicio());
+		statement.setObject(4, turma.getData_termino());
+		statement.setObject(5, turma.getProfessor().getId_professor());
+		
+		statement.execute();
+		
+		connection.close();
 	}
-	public void updateTurma(Turma turma)throws Exception{
-		// TODO
+	public void updateTurma(Turma turma, Professor professor)throws Exception{
+		Connection connection = ConnectionFactory.getConnection();
+
+		PreparedStatement statement = connection.prepareStatement
+				("UPDATE turma SET nome_turma=?, data_inicio=?, data_fim=?, id_professor=? WHERE id_professor=?");
+			
+		statement.setObject(1, turma.getNome_turma());
+		statement.setObject(2, turma.getData_inicio());
+		statement.setObject(1, turma.getData_termino());
+		statement.setObject(1, turma.getProfessor().getId_professor());
+		statement.setObject(1, turma.getId_turma());
+		
+		
+		statement.execute();
+		
+		connection.close();
 	}
 	public void deleteTurma(Turma turma)throws Exception{
-		// TODO
+		
+		Connection connection = ConnectionFactory.getConnection();
+		
+		PreparedStatement statement = connection.prepareStatement
+				("DELETE FROM turma WHERE id_turma=?");
+		
+		statement.setObject(1, turma.getId_turma());
+		
+		statement.execute();
+		
+		
+		
+		connection.close();
+		
 	}
-	public void getAllTurma(Turma turma)throws Exception{
-		// TODO
+	public List<Turma> findAllTurma()throws Exception{
+		
+		Connection connection = ConnectionFactory.getConnection();
+		
+		PreparedStatement statement = connection.prepareStatement
+				("SELECT * FROM turma ORDEER BY nome_turma");
+		
+		ResultSet resultSet = statement.executeQuery();
+		
+		List<Turma> turmas = new ArrayList<Turma>();
+		
+		while(resultSet.next()) {
+			
+			Turma turma = new Turma();
+			turma.setProfessor(new Professor());
+			
+			turma.setId_turma(UUID.fromString(resultSet.getString("id_turma")));
+			turma.setNome_turma(resultSet.getString("nome_turma"));
+			turma.setData_inicio(resultSet.getDate("data_inicio"));
+			turma.setData_termino(resultSet.getDate("data_fim"));
+			turma.getProfessor().setId_professor(UUID.fromString(resultSet.getString("professor_id")));
+			
+			turmas.add(turma);
+		}
+		
+		
+		connection.close();
+		return turmas;
+		
 	}
-	public void getByIdTurma(Turma turma)throws Exception{
-		// TODO
+	public Turma findByIdTurma(UUID id)throws Exception{
+		
+		Connection connection = ConnectionFactory.getConnection();
+		PreparedStatement statement = connection.prepareStatement
+				("SELECT * FROM turma WHERE id_turma=?");
+		statement.setObject(1, id);
+		
+		ResultSet resultSet = statement.executeQuery();
+		
+		Turma turma = null;
+		
+		if(resultSet.next()) {
+			
+			turma = new Turma();
+			turma.setProfessor(new Professor());
+			
+			turma.setId_turma(UUID.fromString(resultSet.getString("id_turma")));
+			turma.setNome_turma(resultSet.getString("nome_turma"));
+			turma.setData_inicio(resultSet.getDate("data_inicio"));
+			turma.setData_termino(resultSet.getDate("data_fim"));
+			turma.getProfessor().setId_professor(UUID.fromString(resultSet.getString("professor_id")));
+			
+			
+		}
+		
+		connection.close();
+		return turma;
 	}
 }
